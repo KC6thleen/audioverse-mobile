af56ac82285a8152662d7215487d8a34fe66236c
Henry Orozco <henry@audioverse.org>
2019-01-09 14:49:41 -0500

    Fix cut off text on some devices

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-import { AppRegistry, Platform } from 'react-native'
+import React from 'react'
+import {
+  AppRegistry,
+  Text,
+  Platform,
+} from 'react-native'
 import TrackPlayer from 'react-native-track-player'
 
 import configureStore from 'src/reducers/configureStore'
@@ -9,3 +14,16 @@ const { store, persistor } = configureStore()
 
 AppRegistry.registerComponent('AudioVerse', () => createApp(store, persistor))
 TrackPlayer.registerPlaybackService(() => createEventHandler(store))
+
+// On android, OnePlus and Oppo devices use a custom font that may cause truncated text on some parts,
+// as a workaround set the default Text font to Roboto or Arial
+// https://github.com/facebook/react-native/issues/15114
+if (Platform.OS === 'android') {
+  const oldRender = Text.render
+  Text.render = function (...args) {
+      const origin = oldRender.call(this, ...args)
+      return React.cloneElement(origin, {
+        style: [{fontFamily: 'Roboto'}, origin.props.style],
+      })
+  }
+}
