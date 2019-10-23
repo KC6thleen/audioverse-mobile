5b32eecb347c89db627fc1eb824d01ddbc661288
Henry Orozco <alangumer@gmail.com>
2019-10-23 16:06:42 -0400

    add Search Navigator

---
+++ src/navigators/SearchNavigator.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { createStackNavigator } from 'react-navigation-stack'
+import { NavigationInjectedProps } from 'react-navigation'
+
+import Search from '../containers/search'
+import Presenter from '../containers/presenters/presenter'
+import Conference from '../containers/conferences/conference'
+import Sponsor from '../containers/sponsors/sponsor'
+import Serie from '../containers/series/serie'
+import { GlobalStyles, headerTintColor } from '../styles'
+
+const Navigator = createStackNavigator({
+  Search,
+  Presenter,
+  Conference,
+  Sponsor,
+  Serie,
+}, {
+  navigationOptions: {
+    header: null,
+  },
+  defaultNavigationOptions: {
+    headerStyle: GlobalStyles.header,
+    headerTintColor: headerTintColor,
+  },
+})
+
+export default Navigator
